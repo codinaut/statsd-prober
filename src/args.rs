acbfb3f9e5dd1f0eff45aad0d204fab65f67d25c
Daniel <codinaut@gmail.com>
2020-07-29 02:25:55 +0700

    refactor: remove individual interval
    BREAKING CHANGE: individual interval isn't supported anymore

--- src/args.rs
+++ src/args.rs
@@ -1,42 +1,27 @@
 use crate::prober::ProbeTarget;
 use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
-use snafu::{ResultExt, Snafu};
+use itertools::Itertools;
 use std::ffi::OsString;
-use std::time::Duration;
-use std::ops::Deref;
-
-#[derive(Debug, Snafu)]
-pub enum Error {
-    Clap { source: clap::Error },
-    ParseDuration { source: humantime::DurationError },
-}
 
 pub struct Arguments {
     targets: Vec<ProbeTarget>,
 }
 
-fn parse_targets<'i, I, V>(indices: I, values: &V) -> Result<Vec<ProbeTarget>, Error>
+fn parse_targets<V>(values: V) -> Vec<ProbeTarget>
 where
-    I: Iterator<Item = &'i usize>,
-    V: Deref<Target = [OsString]>,
+    V: IntoIterator<Item = OsString>,
 {
-    let mut index = 0;
-
-    Ok(indices
-        .map(|u| {
-            let probes = ProbeTarget {
-                address: values[0].clone().into_string().unwrap(),
-                statsd_key: values[1].clone().into_string().unwrap(),
-                interval: Duration::from_secs(1),
-            };
-
-            index += u;
-            probes
+    values
+        .into_iter()
+        .tuples()
+        .map(|(address, statsd_key)| ProbeTarget {
+            address: address.into_string().unwrap(),
+            statsd_key: statsd_key.into_string().unwrap(),
         })
-        .collect())
+        .collect()
 }
 
-pub fn parse_args<I, T>(args: I) -> Result<Arguments, Error>
+pub fn parse_args<I, T>(args: I) -> Arguments
 where
     I: IntoIterator<Item = T>,
     T: Into<OsString> + Clone,
@@ -49,17 +34,18 @@ where
             Arg::with_name("target")
                 .short("t")
                 .help("Specify probe target(s)")
-                .min_values(2)
-                .max_values(3),
+                .takes_value(true)
+                .multiple(true)
+                .number_of_values(2),
         )
-        .get_matches_from_safe(args)
-        .context(Clap)?;
+        .get_matches_from(args);
 
-    Ok(Arguments {
-        targets: matches.args.get("target").map_or(Ok(vec![]), |arg| {
-            parse_targets(arg.indices.iter(), &arg.vals)
-        })?,
-    })
+    Arguments {
+        targets: matches
+            .args
+            .get("target")
+            .map_or(vec![], |arg| parse_targets(arg.vals.clone())),
+    }
 }
 
 #[cfg(test)]
@@ -68,11 +54,25 @@ mod test {
 
     #[test]
     fn parse_first_target() {
-        let arg = parse_args(&["app", "-t", "a", "b"]).unwrap();
-        assert_eq!(arg.targets[0], ProbeTarget{
-            address: "a".to_string(),
-            statsd_key: "b".to_string(),
-            interval: Duration::from_secs(1),
-        })
+        let arg = parse_args(&["app", "-t", "a", "b"]);
+        assert_eq!(
+            arg.targets[0],
+            ProbeTarget {
+                address: "a".to_string(),
+                statsd_key: "b".to_string(),
+            }
+        )
+    }
+
+    #[test]
+    fn parse_next_target() {
+        let arg = parse_args(&["app", "-t", "a", "b", "-t", "c", "d"]);
+        assert_eq!(
+            arg.targets[1],
+            ProbeTarget {
+                address: "c".to_string(),
+                statsd_key: "d".to_string(),
+            }
+        )
     }
 }
--- src/prober.rs
+++ src/prober.rs
@@ -1,8 +1,5 @@
-use std::time::Duration;
-
 #[derive(Debug, PartialEq)]
 pub struct ProbeTarget {
     pub address: String,
     pub statsd_key: String,
-    pub interval: Duration,
 }
