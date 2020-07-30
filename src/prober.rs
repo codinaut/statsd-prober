77d9a5f14d63018437c280ebd44cc9735e10c48e
Daniel <codinaut@gmail.com>
2020-07-30 13:02:12 +0700

    feat: build statsd UDP payload

--- src/prober.rs
+++ src/prober.rs
@@ -10,3 +10,27 @@ pub struct ProbeTarget {
     pub address: String,
     pub statsd_key: String,
 }
+
+fn build_payload(statsd_key: &str) -> Box<[u8]> {
+    format!("{}:1|c", statsd_key)
+        .into_bytes()
+        .into_boxed_slice()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use fake::faker;
+    use fake::Fake;
+
+    #[test]
+    fn build_payload_ok() {
+        let statsd_key = faker::lorem::en::Word().fake();
+        assert_eq!(
+            build_payload(statsd_key),
+            format!("{}:1|c", statsd_key)
+                .into_bytes()
+                .into_boxed_slice()
+        );
+    }
+}
