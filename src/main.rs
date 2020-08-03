7f7eead618c70d81f9aa79e383fd97dfe8a44ef5
Daniel <codinaut@gmail.com>
2020-08-04 03:42:36 +0700

    feat: main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 mod args;
 mod prober;
 
-fn main() {}
+use snafu::{ResultExt, Snafu};
+use std::env;
+use tokio;
+
+#[derive(Debug, Snafu)]
+enum Error {
+    Argument { source: args::Error },
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    prober::Prober::new(args::parse_args(env::args()).context(Argument)?)
+        .probe_all_periodically()
+        .await;
+    Ok(())
+}
